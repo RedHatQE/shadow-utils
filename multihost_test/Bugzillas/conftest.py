447b757b8022f65f92619b02bff40110d982a297
Anuj Borah <aborah@redhat.com>
2021-08-19 13:33:03 +0530

    Tests: support pluggable data sources for subid ranges configurable via /etc/nsswitch.conf
    https://bugzilla.redhat.com/show_bug.cgi?id=1859252

---
+++ multihost_test/Bugzillas/conftest.py
@@ -0,0 +1,126 @@
+"""conftest.py for Shadow-utils"""
+
+from __future__ import print_function
+import subprocess
+import pytest
+import os
+import time
+import ldap
+import random
+from pytest_multihost import make_multihost_fixture
+from sssd.testlib.common.qe_class import session_multihost
+from sssd.testlib.common.paths import SSSD_DEFAULT_CONF
+from sssd.testlib.ipa.utils import ipaTools
+from sssd.testlib.common.utils import PkiTools, sssdTools, LdapOperations
+from sssd.testlib.common.libdirsrv import DirSrvWrap
+from sssd.testlib.common.exceptions import PkiLibException, LdapException
+from sssd.testlib.common.libkrb5 import krb5srv
+
+
+def pytest_configure():
+    """ Namespace hook to add below dict in the pytest namespace """
+    pytest.num_masters = 0
+    pytest.num_ad = 0
+    pytest.num_atomic = 0
+    pytest.num_replicas = 0
+    pytest.num_clients = 1
+    pytest.num_others = 0
+
+
+@pytest.fixture(scope="class")
+def multihost(session_multihost, request):
+    """ Multihost fixture to be used by tests """
+    if hasattr(request.cls(), 'class_setup'):
+        request.cls().class_setup(session_multihost)
+        request.addfinalizer(
+            lambda: request.cls().class_teardown(session_multihost))
+    return session_multihost
+
+
+@pytest.fixture(scope='function')
+def backupsssdconf(session_multihost, request):
+    """ Backup and restore sssd.conf """
+    bkup = 'cp -f %s %s.orig' % (SSSD_DEFAULT_CONF,
+                                 SSSD_DEFAULT_CONF)
+    session_multihost.client[0].run_command(bkup)
+    session_multihost.client[0].service_sssd('stop')
+
+    def restoresssdconf():
+        """ Restore sssd.conf """
+        restore = 'cp -f %s.orig %s' % (SSSD_DEFAULT_CONF, SSSD_DEFAULT_CONF)
+        session_multihost.client[0].run_command(restore)
+    request.addfinalizer(restoresssdconf)
+
+
+@pytest.fixture(scope='class')
+def compile_list_subid_ranges(session_multihost, request):
+    """
+    Compile list_subid_ranges.c file And install
+    necessary packages
+    """
+    session_multihost.client[0].run_command("yum "
+                                            "--enablerepo=rhel-CRB  "
+                                            "install -y "
+                                            "shadow-utils*")
+    session_multihost.client[0].run_command("yum "
+                                            "install -y "
+                                            "shadow-utils-*")
+    session_multihost.client[0].run_command("yum "
+                                            "install -y gcc")
+    session_multihost.client[0].transport.put_file(os.getcwd() +
+                                                   '/data/list_subid_ranges.c',
+                                                   '/tmp/list_subid_ranges.c')
+    session_multihost.client[0].run_command("gcc /tmp/list_subid_ranges.c "
+                                            "-lsubid -o  /tmp/list_subid_ranges")
+
+    def remove():
+        """ Remove file """
+        session_multihost.client[0].run_command("rm -vf /tmp/list_subid_ranges")
+
+    request.addfinalizer(remove)
+
+
+@pytest.fixture(scope='function')
+def create_backup(session_multihost, request):
+    """ Create backup for necessary files used in test """
+    user = "local_anuj"
+    for place in ['subuid', 'subgid']:
+        with pytest.raises(subprocess.CalledProcessError):
+            session_multihost.client[0].run_command(f"grep {user} "
+                                                    f"/etc/{place}")
+    with pytest.raises(subprocess.CalledProcessError):
+        session_multihost.client[0].run_command(f"grep subid "
+                                                f"/etc/nsswitch.conf")
+    session_multihost.client[0].run_command("cp -vf  /etc/subuid "
+                                            "/tmp/subuid_bkp")
+    session_multihost.client[0].run_command("cp -vf  /etc/subgid "
+                                            "/tmp/subgid_bkp")
+    session_multihost.client[0].run_command("cp -vf  /etc/nsswitch.conf "
+                                            "/tmp/nsswitch.conf_bkp")
+
+    def restore():
+        """ Restore files """
+        session_multihost.client[0].run_command("mv -vf  /tmp/subuid_bkp "
+                                                "/etc/subuid")
+        session_multihost.client[0].run_command("mv -vf  /tmp/subgid_bkp "
+                                                "/etc/subgid")
+        session_multihost.client[0].run_command("mv -vf  /tmp/nsswitch.conf_bkp "
+                                                "/etc/nsswitch.conf")
+    request.addfinalizer(restore)
+
+
+@pytest.fixture(scope='function')
+def create_localuser(session_multihost, request):
+    """ Create local users """
+    user = "local_anuj"
+    password = "Secret123"
+    session_multihost.client[0].run_command(f"useradd {user}")
+    passwd_cmd = f'passwd --stdin {user}'
+    session_multihost.client[0].run_command(passwd_cmd,
+                                            stdin_text=password,
+                                            raiseonerr=False)
+
+    def delusers():
+        """ Delete local users """
+        session_multihost.client[0].run_command(f"userdel -rf {user}")
+    request.addfinalizer(delusers)
---
+++ multihost_test/Bugzillas/test_subid_ranges_configurable.py
@@ -0,0 +1,136 @@
+""" Automation of Shadow-utils bugs """
+
+import pytest
+import subprocess
+import time
+import os
+from sssd.testlib.common.utils import SSHClient
+
+
+def execute_cmd(multihost, command):
+    """ execute cmd in client machine """
+    cmd = multihost.client[0].run_command(command)
+    return cmd
+
+
+local_user = "local_anuj"
+test_password = "Secret123"
+subid_start = "165536"
+subid_size = "65536"
+
+
+@pytest.mark.usefixtures('compile_list_subid_ranges')
+class TestSubid(object):
+    """
+    This is for Shadow bugs automation
+    """
+
+    @pytest.mark.parametrize("subid_db",
+                             ["",
+                              "subid: files",
+                              "subid: unexisting"],)
+    def test_subid(self, multihost,
+                   create_backup,
+                   create_localuser,
+                   subid_db):
+        """
+        :Title: support pluggable data sources for
+        subid ranges configurable via /etc/nsswitch.conf
+        :id: 6ff93666-00c0-11ec-9be4-845cf3eff344
+        :bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1859252
+        :steps:
+            1. Define user range in /etc/subuid
+            2. Define group range in /etc/subgid
+            3. Create a process for user
+            4. Check newuidmap command is working
+            with value of subid_db in /etc/nsswitch.conf
+            5. Check newgidmap command is working
+            with value of subid_db in /etc/nsswitch.conf
+        :expectedresults:
+            1. Should succeed
+            2. Should succeed
+            3. Should succeed
+            4. Should succeed
+            5. Should succeed
+        """
+        execute_cmd(multihost,
+                    f"echo '{local_user}:{subid_start}:{subid_size}'"
+                    f"  > /etc/subuid")
+        execute_cmd(multihost,
+                    f"echo '{local_user}:{subid_start}:{subid_size}'"
+                    f"  > /etc/subgid")
+        if subid_db != "":
+            execute_cmd(multihost,
+                        f"echo {subid_db} >> "
+                        f"/etc/nsswitch.conf")
+        ssh1 = SSHClient(multihost.client[0].sys_hostname,
+                         username=local_user,
+                         password=test_password)
+        (results1, results2, results3) = ssh1.exec_command('unshare -U bash')
+        # ps is returning only partial part of user
+        # local_a+ 10131 10105 0 08:30 ? 00:00:00 bash
+        # test looks for a most recent 'bash' process
+        # started by user local_*
+        find_id = "ps -ef | grep bash | grep local_| tail -1"
+        proces_id = [int(word)
+                     for word in execute_cmd(multihost,
+                                             find_id).stdout_text.split()
+                     if word.isdigit()][0]
+        ssh1.exec_command(f"newuidmap {proces_id} "
+                          f"{subid_start}  "
+                          f"{int(subid_start)+1} 1")
+        ssh1.exec_command(f"newgidmap {proces_id} "
+                          f"{subid_start} "
+                          f"{int(subid_start)+1} 1")
+        result = execute_cmd(multihost, f"cat /proc/{proces_id}/uid_map")
+        assert subid_start in result.stdout_text
+        assert str(int(subid_start)+1) in result.stdout_text
+        result = execute_cmd(multihost, f"cat /proc/{proces_id}/gid_map")
+        assert subid_start in result.stdout_text
+        assert str(int(subid_start)+1) in result.stdout_text
+        execute_cmd(multihost, f'kill -9 {proces_id}')
+        ssh1.close()
+
+    @pytest.mark.parametrize("subid_db",
+                             ["",
+                              "subid: files",
+                              "subid: unexisting"],)
+    def test_list_subid_ranges(self, multihost,
+                               create_backup,
+                               create_localuser,
+                               subid_db):
+        """
+        :Title: support pluggable data sources for
+        subid ranges configurable via /etc/nsswitch.conf
+        :id: 496e0142-00c1-11ec-8cb7-845cf3eff344
+        :bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1859252
+        :steps:
+            1. Define user range in /etc/subuid
+            2. Define group range in /etc/subgid
+            3. Create a process for user
+            4. Check list_subid_ranges command is working
+            with value of subid_db in /etc/nsswitch.conf
+            5. Check list_subid_ranges -g command is working
+            with value of subid_db in /etc/nsswitch.conf
+        :expectedresults:
+            1. Should succeed
+            2. Should succeed
+            3. Should succeed
+            4. Should succeed
+            5. Should succeed
+        """
+        execute_cmd(multihost, f"echo '{local_user}:{subid_start}:{subid_size}'"
+                               " > /etc/subuid")
+        execute_cmd(multihost, f"echo '{local_user}:{subid_start}:{subid_size}'"
+                               " > /etc/subgid")
+        if subid_db != "":
+            execute_cmd(multihost, f"echo {subid_db} >>"
+                                   f" /etc/nsswitch.conf")
+        cmd = execute_cmd(multihost, "cd /tmp/; "
+                                     "./list_subid_ranges "
+                                     "local_anuj")
+        assert f'{local_user} {subid_start} {subid_size}' in cmd.stdout_text
+        cmd = execute_cmd(multihost, "cd /tmp/; "
+                                     "./list_subid_ranges"
+                                     " -g local_anuj")
+        assert f'{local_user} {subid_start} {subid_size}' in cmd.stdout_text
